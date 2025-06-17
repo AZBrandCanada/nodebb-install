c88300d6b89d8b514a70a69dd672d44896dc443c
AZBrand.ca <heyafro@gmail.com>
2025-06-17 07:49:51 +0700

    Create ubuntu22.04.sh

---
+++ ubuntu22.04.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg --dearmor -o /usr/share/keyrings/mongodb-server-7.0.gpg
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+mongod --version
+sudo systemctl start mongod
+sudo systemctl status mongod
+mongosh
