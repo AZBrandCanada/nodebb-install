2445372b4485edfb4c2fdef68f42ef9105a7d387
AZBrand.ca <heyafro@gmail.com>
2025-06-17 10:12:05 +0700

    Update ubuntu22.04.sh

--- ubuntu22.04.sh
+++ ubuntu22.04.sh
@@ -3,6 +3,9 @@ sudo apt-get install -y ca-certificates curl gnupg
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg --dearmor -o /usr/share/keyrings/mongodb-server-7.0.gpg
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
+curl -fsSL https://deb.nodesource.com/setup_lts.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
 sudo apt-get install -y mongodb-org
 mongod --version
 sudo systemctl start mongod
